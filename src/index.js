83e8b689399d7f3c2327f020f56d5dc06b1996a8
Nick Prodromou <hello@nickpro.com.au>
2018-07-06 16:32:30 +1000

    Feature/set up parcel and install react (#1)
    * add .gitignore
    
    * initial bare setup with dev & build

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div>App works</div>
+)
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app-target')
+)
